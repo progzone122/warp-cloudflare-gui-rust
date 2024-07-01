735af733d748fc4c4da4011e8f04d54110b46dc3
progozone122 <80642969+progzone122@users.noreply.github.com>
2024-07-02 02:01:19 +0300

    add settings, add set default switch status, set backend Qt, fix others...

--- build.rs
+++ build.rs
@@ -3,6 +3,13 @@ fn main() {
         .with_style("fluent-dark".into());
     let config_dialog = slint_build::CompilerConfiguration::new()
         .with_style("fluent-dark".into());
+    let config_settings = slint_build::CompilerConfiguration::new()
+        .with_style("fluent-dark".into());
+
+    // set slint backend
+    println!(
+        "cargo:rustc-env=SLINT_BACKEND=Qt"
+    );
 
     println!("cargo:warning================================== compile build.rs =================================\n");
     if slint_build::compile_with_config("ui/app.slint", config_app).is_ok() {
@@ -18,5 +25,12 @@ fn main() {
             std::env::var("OUT_DIR").unwrap()
         );
         println!("cargo:warning=compile ui/dialog.slint success\n");
+    }
+    if slint_build::compile_with_config("ui/settings.slint", config_settings).is_ok() {
+        println!(
+            "cargo:rustc-env=SLINT_INCLUDE_SETTINGS={}/settings.rs",
+            std::env::var("OUT_DIR").unwrap()
+        );
+        println!("cargo:warning=compile ui/settings.slint success\n");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod win1 {
-    include!(env!("SLINT_INCLUDE_APP"));
+    include!(concat!(env!("OUT_DIR"), "/app.rs"));
 }
 mod win2 {
-    include!(env!("SLINT_INCLUDE_DIALOG"));
+    include!(concat!(env!("OUT_DIR"), "/dialog.rs"));
 }
+mod win3 {
+    include!(concat!(env!("OUT_DIR"), "/settings.rs"));
+}
+
 slint::include_modules!();
-use std::os::linux::raw::stat;
 use win1::AppWindow;
 use win2::DialogWindow as CustomDialogWindow;
+use win3::SettingsWindow as CustomSettingsWindow;
 
 mod api;
 
@@ -17,12 +21,13 @@ use slint::{SharedString, Weak};
 fn show_dialog(weak: &Weak<CustomDialogWindow>, dialog_type: String, text: String) {
     weak.unwrap().set_dialog_type(SharedString::from(dialog_type));
     weak.unwrap().set_dialog_text(SharedString::from(text));
-    weak.unwrap().show();
+    weak.unwrap().show().expect("Error: Failed to show window");
 }
 
 fn main() -> Result<(), slint::PlatformError> {
     let app = AppWindow::new()?;
     let dialog = CustomDialogWindow::new()?;
+    let settings = CustomSettingsWindow::new()?;
 
     let api = Api::new();
     app.set_window_title(SharedString::from("WARP"));
@@ -30,11 +35,25 @@ fn main() -> Result<(), slint::PlatformError> {
     let app_weak = app.as_weak();
     let dialog_weak = dialog.as_weak();
     let dialog_weak2 = dialog.as_weak();
+    let setting_weak = settings.as_weak();
 
+    // Set default switch On/Off value
+    if app_weak.unwrap().get_connection_switch_enabled() {
+        let connected_status: bool = api.is_connected();
+        app_weak.unwrap().set_connection_switch_checked(connected_status);
+
+        if connected_status {
+            app_weak.unwrap().set_status(SharedString::from("Connected"));
+        } else {
+            app_weak.unwrap().set_status(SharedString::from("Disconnected"));
+        }
+    }
+
+    // Switch On/Off connection
     app.on_connection_switch_toggled(move || {
         if app_weak.unwrap().get_connection_switch_enabled() {
             if app_weak.unwrap().get_connection_switch_checked() {
-                if api.disconnect() == true {
+                if api.disconnect() {
                     app_weak.unwrap().set_status(SharedString::from("Disconnected"));
                     app_weak.unwrap().set_connection_switch_checked(false);
                 } else {
@@ -43,7 +62,7 @@ fn main() -> Result<(), slint::PlatformError> {
                     show_dialog(&dialog_weak, "error".to_string(), status);
                 }
             } else {
-                if api.connect() == true {
+                if api.connect() {
                     if api.is_connected() {
                         app_weak.unwrap().set_status(SharedString::from("Connected"));
                         app_weak.unwrap().set_connection_switch_checked(true);
@@ -62,6 +81,12 @@ fn main() -> Result<(), slint::PlatformError> {
         }
     });
 
+    // Clicked the settings button
+    app.on_settings_clicked_button(move || {
+        setting_weak.unwrap().show().expect("Error: Failed to show window");
+    });
+
+    // Close dialog
     dialog.on_dialog_clicked_ok(move || {
         dialog_weak2.unwrap().hide().expect("Error: Failed to hide window");
     });
